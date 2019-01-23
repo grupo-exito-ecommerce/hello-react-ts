51aa0058cad0ea4917c8502bc603921d6c015a82
Maicol Stiven Restrepo Alvarez <stiven3345@hotmail.com>
2019-01-23 08:42:48 -0500

    Actualización typings

--- react/typings/exito/exito.icons.d.ts
+++ react/typings/exito/exito.icons.d.ts
@@ -1,6 +1,12 @@
 declare module 'exito.icons' {
   import { ReactElement } from 'react';
-  export const Icon: ReactElement<any>;
+  export const Icon: ReactElement<IconModel>;
   export const App: ReactElement<any>;
-  export const IconList: ReactElement<any>
+  export const IconList: ReactElement<any>;
+}
+interface IconModel {
+  icon: string;
+  size?: string;
+  color?: string;
+  viewBox?: string;
 }
--- react/typings/vtex/render.d.ts
+++ react/typings/vtex/render.d.ts
@@ -1,33 +1,30 @@
 /* Typings for `render-runtime` */
 declare module 'render' {
-  import { Component, ReactElement } from 'react'
+  import { ReactElement, Component } from 'react';
 
-  export const ExtensionPoint: ReactElement
-  export const Helmet: ReactElement
-  export const Link: ReactElement
-  export const NoSSR: ReactElement
-  export const RenderContextConsumer: ReactElement
-  export const canUseDOM: boolean
+  export const ExtensionPoint: ReactElement;
+  export const Link: ReactElement;
+  export const NoSSR: ReactElement;
+  export const RenderContextConsumer: ReactElement;
+  export const canUseDOM: boolean;
   export const withRuntimeContext: <TOriginalProps extends {}>(
     Component: ComponentType<TOriginalProps & RenderContextProps>
-  ) => ComponentType<TOriginalProps>
-
-  export const buildCacheLocator = (app: string, type: string, cacheId: string) => string
+  ) => ComponentType<TOriginalProps>;
 
   interface RenderComponent<P = {}, S = {}> extends Component<P, S> {
-    getCustomMessages?: (locale: string) => any
-    schema: ComponentSchema
-    getSchema?: (a: any, b: any?) => ComponentSchema
-    uiSchema: UISchema
+    getCustomMessages?: (locale: string) => any;
+    schema: ComponentSchema;
+    getSchema?: (any, any?) => ComponentSchema;
+    uiSchema: UISchema;
   }
 
   export interface ComponentsRegistry {
-    [component: string]: RenderComponent<any, any>
+    [component: string]: RenderComponent<any, any>;
   }
 
   export interface Window extends Window {
-    __RENDER_7_COMPONENTS__: ComponentsRegistry
+    __RENDER_7_COMPONENTS__: ComponentsRegistry;
   }
 
-  let global: Window
+  var global: Window;
 }
