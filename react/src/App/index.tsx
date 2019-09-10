d66ec8cb0b28a75404a2ce4abf31dad03d60827d
Maik Restrepo <stiven3345@hotmail.com>
2019-09-10 10:48:35 -0500

    Update dependencies and tslint file

--- react/src/App/index.tsx
+++ react/src/App/index.tsx
@@ -1,7 +1,7 @@
 import classnames from 'classnames'
 import React from 'react'
-import { FormattedMessage } from 'react-intl'
 import styles from './App.css'
+import { FormattedMessage } from "react-intl"
 
 const App = () => {
   return (
