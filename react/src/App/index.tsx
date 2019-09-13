c784b79a02c8d4b3ff86e3aadded3e0718c130ff
José Ángel Bermúdez Puello <jbermudezp>
2019-09-13 13:29:16 -0500

    configuración de reglas de desarrollo con tslint.json

--- react/src/App/index.tsx
+++ react/src/App/index.tsx
@@ -1,25 +1,27 @@
-import classnames from 'classnames'
-import React from 'react'
-import { FormattedMessage } from 'react-intl'
-import styles from './App.css'
+import classnames from 'classnames';
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import styles from './App.css';
 
 const App = () => {
   return (
     <div className={classnames(styles.App)}>
-     
       <header className={classnames(styles.AppHeader)}>
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2000px-React-icon.svg.png"
           className={classnames(styles.AppLogo)}
           alt="logo"
         />
-        <h1 className={classnames(styles.AppTitle)}><FormattedMessage id="welcome" /></h1>
+        <h1 className={classnames(styles.AppTitle)}>
+          <FormattedMessage id="welcome" />
+        </h1>
       </header>
       <p className={classnames(styles.AppIntro)}>
-        <FormattedMessage id="getstarted" /> <code>src/App.tsx</code>  <FormattedMessage id="saveTo" />
+        <FormattedMessage id="getstarted" /> <code>src/App.tsx</code>{' '}
+        <FormattedMessage id="saveTo" />
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
