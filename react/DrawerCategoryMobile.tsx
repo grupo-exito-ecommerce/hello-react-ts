1532fdd58198f1871647939202cf2f6cf4ec38d4
Michael Stiven Restrepo Alvarez <michael.restrepo@puntoscolombia.com>
2020-08-09 15:47:06 -0500

    Remove unused component

--- react/DrawerCategoryMobile.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CategoryButton from './src/components/CategoryMenu/components/CategoryButton';
-
-const DrawerCategoryMobile = () => {
-  return <CategoryButton {...{ isMobile: true }} />;
-};
-
-export default DrawerCategoryMobile;
